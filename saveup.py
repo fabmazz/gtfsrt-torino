dfbfed12906caf9179f3d2c3fb42606f320f1f5f
Fabio Mazza <fab4mazz@gmail.com>
2020-12-07 01:32:49 +0100

    fix saving

--- saveup.py
+++ saveup.py
@@ -1,5 +1,7 @@
 import sys
 import gtt_updates
+import time
+import json
 
 BASE_NAME_OUT = "updates_{}.json.csv"
 
@@ -17,27 +19,37 @@ class Update:
 
     def __hash__(self):
         return hash((self.timest, self.veh_num, self.route))
+    
+    def __eq__(self, other):
+        return self.timest == other.timest and self.veh_num == other.veh_num and self.route == other.route
 
     def __str__(self):
         return f"ts: {self.timest}, veh: {self.veh_num}, route: {self.route}"
 
 def get_all_updates():
     data = gtt_updates.get_updates()
-
+    print(data.keys())
     all_up = [Update(d) for d in data["entity"]]
 
     return all_up
 
 def main(argv):
 
+    starttime = time.time()
     r = get_all_updates()
 
     v = set(r)
+    print(len(v))
+    time.sleep(2)
+    n = set(get_all_updates())
 
-    for x in r:
-        print(x)
+    v.update(n)
 
-    print(len(r), len(v))
+    print(len(v))
+    with open(sys.argv[1], "w") as f:
+        d= [l.data for l in v]
+        json.dump(d, f, indent=3)
 
+    print([hash(l) for l in v])
 if __name__ == '__main__':
     main(sys.argv)
